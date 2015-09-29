7d0ae253cb7027a0cc7173c0519b9cb6522f0b35
Andrew Kim <akim27@student.rcc.edu>
2015-09-29 04:08:08 +0000

    Fixed Problem 4

--- Hmwk/Multiply-Shift/homework4.s
+++ Hmwk/Multiply-Shift/homework4.s
@@ -8,8 +8,9 @@
 .global _start
 _start:
 	MOV R1, #6		@BP 0  WD 3
-	LDR R2, =0x003243f6	@BP-20 WD 24
-	MUL R0, R2, R1		@BP-20 WD 27
-	LSR R0, #20
+	LDR R2, =0x03243F6A	@BP-20 WD 24
+	MUL R3, R1, R1		@BP 0  WD 6
+	MUL R0, R2, R3		@BP-20 WD 30 
+	LSR R0, #24
 	MOV R7, #1
 	SWI 0

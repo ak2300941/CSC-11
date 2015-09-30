a8192a81789a87c772aeacaf0e77b228f5f85bbd
Andrew Kim <akim27@student.rcc.edu>
2015-09-30 19:19:16 +0000

    Division Mod Homework

--- Hmwk/DivMod1/homework1.s
+++ Hmwk/DivMod1/homework1.s
@@ -5,6 +5,21 @@
 	Divide using subtration and loops only
 */
 
-.global _start:
+.global _start
 _start:
-	
+	/*Initialize the Registers*/
+	MOV R1, #0		@Result of R3/R2
+	MOV R2, #6		@Denominator
+	MOV R3, #55		@Numerator
+	/*Loop and Keep Subtracting Off the Denominator*/
+	MOV R0, R3		@Place the Numerator in R0
+	_while_R0_GE_R2:	@Loop until the Division is over
+	CMP R0, R2		@Compare R0 and R2
+	BLT _output		@Exit and Output the Result
+		ADD R1, R1, #1	@Increment the Division Counter
+		SUB R0, R0, R2	@Subtract the Denominator from the Numerator
+	BAL _while_R0_GE_R2
+	/*Output the Result*/
+	_output:
+	MOV R7, #1
+	SWI 0

cea88d2c6e80ab3368d506226751399bd8f2afb6
Andrew Kim <akim27@student.rcc.edu>
2015-10-18 00:23:50 +0000

    Finished Loops

--- Hmwk/Constructs/while.s
+++ Hmwk/Constructs/while.s
@@ -1,17 +1,19 @@
 /*
         Andrew Kim
         October 17th, 2015
-        Creating do Loop
+        Creating While Loop
 */
 
 .global _start
 _start:
-	MOV R0, #0		@Initialize/Give #0
+	MOV R0, #0 		@Initialize/Give #0
 	MOV R1, #1		@Counter
-doloop:
+whileloop:
+	CMP R1, #10		@Compare
+	BGT end			@R1>10 go to end
 	ADD R0, R0, R1		@Add +1 to R0
 	ADD R1, R1, #1		@Increment Counter
-	CMP R1, #10		@Compare
-	BLE doloop		@R1 <= 10
+	BAL whileloop		@Move back to whileloop
+end:
 	MOV R7, #1
 	SWI 0

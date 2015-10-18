b177cefc41ce7d3e6cc0af4ac3514edcce9864b7
Andrew Kim <akim27@student.rcc.edu>
2015-10-18 00:15:32 +0000

    Finished 1

---
+++ Hmwk/Constructs/do.s
@@ -0,0 +1,12 @@
+/*
+        Andrew Kim
+        October 17th, 2015
+        Creating do Loop
+*/
+
+.global _start
+_start:
+	MOV R0, #0		@Initialize
+	MOV R1, #1		@Counter
+doloop:
+	ADD R0, R0, R1		@

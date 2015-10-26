f316382b6070506e256cd428281386df6d678d1e
Andrew Kim <akimzet@CSC11.(none)>
2015-10-26 12:26:06 -0700

    Added more

--- Hmwk/ExHomework/main.s
+++ Hmwk/ExHomework/main.s
@@ -20,6 +20,14 @@ message2: .asciz "If range is degree Centigrade input 1: "
 .balign 4
 message3: .asciz "If range is degree Fagrenheit input 2: "
 
+/* Message 4 */
+.balign 4
+message4: .asciz "Display Degree Centigrade to Degree Fahrenheit\n"
+
+/* Message 5 */
+.balign 4
+message5: .asciz "Display Degree Fahrenheit to Degree Centigrade\n"
+
 /* Message to Check Input */
 .balign 4
 testmsg: .asciz "%d and %d\n"
@@ -31,7 +39,7 @@ testcho: .asciz "Choice is %d\n"
 
 /* Takes in 2 Inputs */
 .balign 4
-scan_pattern1: .asciz "%d, %d"
+scan_pattern1: .asciz "%d %d"
 
 /* Take in 1 Input */
 .balign 4
@@ -56,11 +64,11 @@ main:
 	BL printf				@Call printf
 	/* Call scanf, and pass address of format */
 	LDR R0, address_of_scan_pattern1	@R0 <- &scan_pattern1
-	LDR R1, address_of_beg			@R1 <- Ready to store
-	LDR R2, address_of_end			@R2 <- Ready to store
+	LDR R1, address_of_beg			@R1 <- Stored into
+	LDR R2, address_of_end			@R2 <- Stored into
 	BL scanf				@Call to scanf
-	LDR R1, address_of_beg			@R1 <- Store #
-	LDR R2, address_of_end			@R2 <- Store #
+	LDR R1, address_of_beg			@R1 <- Reloading after function
+	LDR R2, address_of_end			@R2 <- Reloading after function
 	LDR R1, [R1]
 	LDR R2, [R2]
 	LDR R0, address_of_testmsg		@R0 <- &testmsg
@@ -73,18 +81,32 @@ main:
 	BL printf				@Call printf
 	/* Call scanf, and pass address of format */
 	LDR R0, address_of_scan_pattern2	@R0 <- &scan_pattern2
-	LDR R3, address_of_choice		@R3 <- Ready to store
+	LDR R1, address_of_choice		@R3 <- Ready to store
 	BL scanf				@Call to scanf
-	LDR R3, address_of_choice		@R3 <- Store #
-	LDR R3, [R3]
+	LDR R1, address_of_choice		@R3 Reloading after function
+	LDR R1, [R1]
 	LDR R0, address_of_testcho		@R0 <- &testcho
 	BL printf
-	/* Exit */
+	LDR R0, address_of_choice		@Reload code to use in another function
+	LDR R0, [R0]				@Load Register value inside the address
+	CMP R0, #1				@Compare
+	BEQ Problem1				@R0 = 1 Go to Problem1
+	BGT Problem2				@R0 > 1 Go to Problem2
+Problem1:
+	LDR R0, address_of_message4
+	BL printf
+	BX LR
+
+Problem2:
+	LDR R0, address_of_message5
+	BL printf
 	BX LR
 
 address_of_message1: .word message1
 address_of_message2: .word message2
 address_of_message3: .word message3
+address_of_message4: .word message4
+address_of_message5: .word message5
 address_of_testmsg: .word testmsg
 address_of_testcho: .word testcho
 address_of_scan_pattern1: .word scan_pattern1

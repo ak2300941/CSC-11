357887a5357563bd97faa1a0f6006448a6b5d5b3
ak2300941 <akim27@student.rcc.edu>
2015-12-13 16:09:19 -0800

    Signed-off-by: ak2300941 <akim27@student.rcc.edu>

--- Final/Final_Assembly_Code/mod.s
+++ Final/Final_Assembly_Code/mod.s
@@ -1,47 +1,67 @@
-/*
-	Andrew Kim
-	September 30th, 2015
-	Div/Mod Shift
-	Use the Shifted Subtraction Method
+/* 
+	Functions
+		scaleRight
+		addSub
+		scaleLeft
+		divMod
 */
 
-.global _start
-_start:
-	/*Initialize The Registers*/
-	MOV R0, #0		@Result of R5%R4 start with Numerator
-	MOV R1, #0		@Result of R5/R4 start with 0
-	MOV R2, #1		@Increment Value start with 1
-	MOV R3, #0		@Decrement Value start with Denominator
-	/*Declaration of Variables Holding Inputs*/
-	MOV R4, #10		@Denominator
-	MOV R5, #21		@Numerator
-	/*Initialize the Working Registers with the Data*/
-	MOV R0, R5
-	MOV R3, R4
-shiftleft:
-	/*Shift the Denominator left until greater than Numerator, then shift back*/
-	LSL R3, #1		@Denominator Shift Left
-	LSL R2, #1		@Division Shift Left
-	CMP R0, R3		@Compare
-	BGT shiftleft		@R0 > R3
-	LSR R3, #1		@Shift Denominator Right
-	LSR R2, #1		@Shift Division Right
-subtract:
-	CMP R0, R3		@Compare
-	BLT output		@R0 < R3
-	ADD R1, R1, R2		@Increment division by the increment
-	SUB R0, R0, R3		@Subtract shifted Denominator with remainder of Numerator
-	/*Shift right until denominator is less than numerator*/
-shiftright:
-	CMP R2, #1		@Compare
-	BEQ subtract		@R2 == 1
-	CMP R3, R0		@Compare
-	BLE subtract		@R3 <= R0
-	LSR R2, #1		@Shifted Increment Right
-	LSR R3, #1		@Shifted Denominator Left
-	BAL shiftright		@Shift Denominator until less than Numerator
-	BAL subtract		@Keep Looping until the division is complete
-output:
-	MOV R0, R1		@See Output on 21/10
-	MOV R7, #1
-	SWI 0
+.text
+
+/*void scaleRight(int &r1,int &r3,int &r2) */
+.globl scaleRight
+scaleRight:
+	push {lr}             /* Push lr onto the stack */
+	doWhile_r1_lt_r2:     /* Shift right until just under the remainder */
+		mov r3,r3,ASR #1; /* Division counter */
+		mov r2,r2,ASR #1  /* Mod/Remainder subtraction */
+	cmp r1,r2
+	blt doWhile_r1_lt_r2
+	pop {lr}              /* Pop lr from the stack */
+    bx lr                 /* Leave scaleRight */
+/* end scaleRight */
+
+/* void addSub(int &r3,int &r2,int &r0,int &r1) */
+.globl addSub
+addSub:
+	push {lr}       /* Push lr onto the stack */
+	doWhile_r3_ge_1:
+		add r0,r0,r3
+		sub r1,r1,r2
+		bl scaleRight
+	cmp r3,#1
+	bge doWhile_r3_ge_1
+    pop {lr}       /* Pop lr from the stack */
+    bx lr          /* Leave addSub */
+/* end addSub */
+
+/* void scaleLeft(int &r1,int &r3,int &r2) */
+.globl scaleLeft
+scaleLeft:
+	push {lr}             /* Push lr onto the stack */
+	doWhile_r1_ge_r2:     /* Scale left till overshoot with remainder */
+		mov r3,r3,LSL #1  /* scale factor */
+		mov r2,r2,LSL #1  /* subtraction factor */
+		cmp r1,r2
+	bge doWhile_r1_ge_r2  /* End loop at overshoot */
+	mov r3,r3,ASR #1      /* Scale factor back */
+	mov r2,r2,ASR #1      /* Scale subtraction factor back */
+	pop {lr}              /* Pop lr from the stack */
+    bx lr                 /* Leave addSub */
+/* end scaleLeft */
+
+/* void divMod(int &r2,int &r0,int &r1) */
+.globl divMod
+divMod:
+	push {lr}       /* Push lr onto the stack */
+	/* Determine the quotient and remainder */
+	mov r0,#0
+	mov r3,#1
+	cmp r1,r2
+	blt end
+		bl scaleLeft
+		bl addSub
+	end:
+	pop {lr}       /* Pop lr from the stack */
+    bx lr          /* Leave addSub */
+/* end divMod */
